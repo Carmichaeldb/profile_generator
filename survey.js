0c21aad9c42bb4d70be1928257a69627a8af0831
David Carmichael <carmichael.db88@gmail.com>
2023-10-15 04:19:09 +0000

    completed survey assignment

---
+++ survey.js
@@ -0,0 +1,34 @@
+/**
+ * ask users questions like favourite music, activity, food, sport, etc
+ * will generate a profile description based on the answers
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              console.log(`${answer} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+
+
+
+
